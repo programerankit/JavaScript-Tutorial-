e38e26f92f4391ed5fc2699f2018117c6a27464d
Ankit Kumar <115234172+programerankit@users.noreply.github.com>
2024-03-01 09:40:52 +0000

    Conversion

---
+++ 04forth/index.js
@@ -0,0 +1,30 @@
+//please ignor the spaling mistack
+
+//In this video we will descuse how to convert a data item into another data item
+// let str="90";
+// let str="933dd" 
+//  let str="ankit";
+// let strNumber= Number(str);
+// console.table([typeof strNumber, strNumber])
+
+
+// let num=90;
+// let numString=String(num)
+//console.table([typeof numString, strNumber])
+
+//boolen to in
+//let isTogale=true;
+//let isTogale=false;
+//let Togale=Number(isTogale)
+//console.log(Togale)
+
+
+// let abc="";
+//let abc='abc'
+// let abcBool=Boolean(abc)
+// console.log(abcBool)
+
+// let abc=null
+// let abcNumber=Number(abc)
+// console.log(abcNumber)
+
