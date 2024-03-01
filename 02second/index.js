2db18ea57c01ca12fcbeb8d2062a9459f25dac8a
Ankit Kumar <115234172+programerankit@users.noreply.github.com>
2024-03-01 05:25:12 +0000

    let, var, const

---
+++ 02second/index.js
@@ -0,0 +1,6 @@
+//In this tutorial we will descuss diffrence between const, let ,var 
+const cons=90;  //This is constant variable the inislize at declare time and we can't redeclare, and re asigned
+let name='AKSD SOLUTION';
+var address='this is address';
+de='de';    //this is also valid but it is not faire
+console.table([cons,name,address,de])  //use the console.table for see the table view index -> value
